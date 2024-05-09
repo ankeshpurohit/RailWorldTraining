3ec8342e13383716b8ec698368c097af9e4e9df5
Pawan-Kushwaha <169117942+Pawan-Kushwaha@users.noreply.github.com>
2024-05-09 22:33:17 +0530

    Electricity Bill

--- ElectricityBill.java
+++ ElectricityBill.java
@@ -1,3 +1,5 @@
+package ElectritiBill.src;
+
 import java.util.Scanner;
 
 public class ElectricityBill {
@@ -5,15 +7,15 @@ public class ElectricityBill {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any Units :");
-        int units = sc.nextInt();
+        int ut = sc.nextInt();
         double ans;
-        ans=(units<=100) ? units*2 :
-                (units<=150) ? (100*2)+(units-100)*3 :
-                        (units<=200) ? (100*2)+(50*3)+(units-150)*4 :
-                                (units<=250) ? (100*2)+(50*3)+(50*4)+(units-200)*5:
-                                        (units<=300) ? (100*2)+(50*3)+(50*4)+(50*5)+(units-250)*6:
-                                                (units<=500) ? (100*2)+(50*3)+(50*4)+(50*5)+(50*6)+(units-300)*7:
-                                                        (100*2)+(50*3)+(50*4)+(50*5)+(50*6)+(50*7)+(units-500)*8;
+        ans=(ut<=100) ? ut*2 :
+                (ut<=150) ? (100*2)+(ut-100)*3 :
+                        (ut<=200) ? (100*2)+(50*3)+(ut-150)*4 :
+                                (ut<=250) ? (100*2)+(50*3)+(50*4)+(ut-200)*5:
+                                        (ut<=300) ? (100*2)+(50*3)+(50*4)+(50*5)+(ut-250)*6:
+                                                (ut<=500) ? (100*2)+(50*3)+(50*4)+(50*5)+(50*6)+(ut-300)*7:
+                                                        (100*2)+(50*3)+(50*4)+(50*5)+(50*6)+(50*7)+(ut-500)*8;
 
         System.out.println(ans);
     }
